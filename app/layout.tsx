a6cfe1f0ee6d2f600fab624b7db8da0b75c9c20d
Kenneth Nnabuife <nnabuifekenneth766@gmail.com>
2025-10-30 12:21:11 +0000

    added GA script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,9 +17,6 @@ export const metadata: Metadata = {
   title: "LinksProbe - Safe Link Checker",
   description:
     "LinksProbe is an open source web app that checks if a link is safe, suspicious, or dangerous using Google’s Safe Browsing API.",
-  other: {
-    "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION!,
-  },
 };
 
 export default function RootLayout({
@@ -32,6 +30,20 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* My Google Analytics setup */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-F51JZ919WL"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-setup" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F51JZ919WL');
+          `}
+        </Script>
       </body>
     </html>
   );
